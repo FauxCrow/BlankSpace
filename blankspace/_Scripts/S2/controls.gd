408581e0c8f295b7833327ca19da13b5cb37912c
hhafzahh <2001231F@student.tp.edu.sg>
2024-10-17 13:02:38 +0800

    UI Assets
    - Created happiness bar icons
    - worked a bit on journal

--- blankspace/_Scripts/S2/controls.gd
+++ blankspace/_Scripts/S2/controls.gd
@@ -3,11 +3,18 @@ extends Node
 @export var controlPanel: ColorRect
 @export var energyBar: TextureProgressBar
 @export var happinessBar: TextureProgressBar
+@export var happinessIcon: TextureRect
+
+@export var happyIcons: Array[Texture]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameInfo.happinessTimer.start()
 	GameInfo.rechargeTimer.start()
+	
+	happyIcons.append(load("res://_UI/Sprites/happyIcon1.png"))
+	happyIcons.append(load("res://_UI/Sprites/neutralIcon2.png"))
+	happyIcons.append(load("res://_UI/Sprites/angryIcon3.png"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,5 +22,12 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("(S2) ToggleDashboard"):
 		controlPanel.visible = !controlPanel.visible
 	
-	energyBar.value = GameInfo.energy
-	happinessBar.value = GameInfo.happiness
+	energyBar.set_value_no_signal(GameInfo.energy)
+	happinessBar.set_value_no_signal(GameInfo.happiness)
+	
+	if happinessBar.value > 80:
+		happinessIcon.texture = happyIcons[0]
+	elif happinessBar.value > 20:
+		happinessIcon.texture = happyIcons[1]
+	else:
+		happinessIcon.texture = happyIcons[2]
